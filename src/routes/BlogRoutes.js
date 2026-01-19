6f994bfd94db715be7805fcee13583506ad7bd53
Farhan Ishraq <farhanishraq777@gmail.com>
2026-01-20 01:23:36 +0600

    added blogroute

---
+++ src/routes/BlogRoutes.js
@@ -0,0 +1,53 @@
+// routes/BlogRoutes.js
+import express from 'express';
+import { VerifyToken } from '../middlewares/VeriyToken.js';
+import {
+  CreateBlog,
+  GetAllBlogs,
+  GetSingleBlog,
+  UpdateBlog,
+  DeleteBlog,
+  ToggleLikeBlog,
+  IncrementBlogView,
+  GetMyBlogs,
+  GetPopularBlogs,
+} from '../controllers/BlogController.js';
+import {
+  AddComment,
+  GetBlogComments,
+  GetCommentReplies,
+  UpdateComment,
+  DeleteComment,
+  ToggleLikeComment,
+} from '../controllers/BlogCommentController.js';
+
+const router = express.Router();
+
+// ========== BLOG ROUTES ==========
+
+// Public routes (anyone can view blogs)
+router.get('/', GetAllBlogs);
+router.get('/popular', GetPopularBlogs);
+router.get('/:id', GetSingleBlog);
+router.patch('/:id/view', IncrementBlogView);
+
+// Protected routes (require authentication)
+router.post('/', VerifyToken, CreateBlog);
+router.put('/:id', VerifyToken, UpdateBlog);
+router.delete('/:id', VerifyToken, DeleteBlog);
+router.patch('/:id/like', VerifyToken, ToggleLikeBlog);
+router.get('/user/my-blogs', VerifyToken, GetMyBlogs);
+
+// ========== COMMENT ROUTES ==========
+
+// Public routes (anyone can view comments)
+router.get('/:blogId/comments', GetBlogComments);
+router.get('/comments/:commentId/replies', GetCommentReplies);
+
+// Protected routes (require authentication)
+router.post('/:blogId/comments', VerifyToken, AddComment);
+router.put('/comments/:commentId', VerifyToken, UpdateComment);
+router.delete('/comments/:commentId', VerifyToken, DeleteComment);
+router.patch('/comments/:commentId/like', VerifyToken, ToggleLikeComment);
+
+export default router;
